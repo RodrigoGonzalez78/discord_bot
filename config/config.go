532b64acabff2a73264bfee8db577104516e75f4
RodrigoGonzalez78 <61030017+RodrigoGonzalez78@users.noreply.github.com>
2022-05-26 22:49:50 -0300

    inicio

---
+++ config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+var (
+	Token     string
+	BotPrefix string
+
+	config *configStruct
+)
+
+type configStruct struct {
+	Token     string `json:"Token"`
+	BotPrefix string `json:"BotPrefix"`
+}
+
+//ReadConfig lee el archivo json de configuracion
+func ReadConfig() error {
+
+	fmt.Println("Leyendo archivos de configuracion")
+
+	file, err := ioutil.ReadFile("./config.json")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println(string(file))
+
+	err = json.Unmarshal(file, &config)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	Token = config.Token
+	BotPrefix = config.BotPrefix
+
+	return nil
+}
